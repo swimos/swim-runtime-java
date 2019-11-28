30e4b0ab0884e2b0299c6ac43748df9c434d31c9
SirCipher <tklapwijk92@gmail.com>
2019-11-28 11:53:38 +0000

    Adds dummy spec for swim.io.http

---
+++ swim-core-java/swim.io.http/src/test/java/swim/io/http/DummySpec.java
@@ -0,0 +1,12 @@
+package swim.io.http;
+
+import org.testng.annotations.Test;
+
+public class DummySpec {
+
+  @Test
+  public void dummy(){
+    // I'm only here so that the module doesn't fail when testing due to finding no tests
+  }
+
+}
